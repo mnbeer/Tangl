4d8029b5251fb60f31b5871204035ecbfffbbf66
Matthew Beer <matthew@rocketsocks.com>
2020-09-20 11:16:34 -0700

    v1.2.0

--- TanglAnalyzer/TanglAnalyzer/PublishToNuget.ps1
+++ TanglAnalyzer/TanglAnalyzer/PublishToNuget.ps1
@@ -1,3 +1,3 @@
 ﻿$apiKey=$args[0]
-dotnet pack --Configuration Release
-dotnet nuget push bin\MCD\release\TanglAnalyzer.1.1.0.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
+dotnet pack --configuration Release
+dotnet nuget push bin\MCD\release\TanglAnalyzer.1.2.0.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
