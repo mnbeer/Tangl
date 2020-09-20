5f9e1a63dd443d85d64d7a6a61889a90c9f5024c
Matthew Beer <matthew.beer@foundrysoftware.com>
2020-09-20 14:44:41 -0700

    Update NuGet package creation.

--- TanglAnalyzer/TanglAnalyzer/PublishToNuget.ps1
+++ TanglAnalyzer/TanglAnalyzer/PublishToNuget.ps1
@@ -1,3 +1,4 @@
 ﻿$apiKey=$args[0]
-dotnet pack --configuration Release
-dotnet nuget push bin\MCD\release\TanglAnalyzer.1.2.0.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
+#dotnet pack --configuration Release
+dotnet build --configuration Release
+dotnet nuget push bin\release\TanglAnalyzer.1.2.1.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
